c00e0470e45fdfc36df18f6a6a89ab0ce6fc1f7f
Hubert <hubertrzeminski16@gmail.com>
2020-11-14 10:56:50 +0000

    Player class added

--- src/Player.java
+++ src/Player.java
@@ -1,5 +1,29 @@
-public class Main(){
-  public static void main(String[] args){
-    System.out.println("Hello world!");
+import java.util.ArrayList;
+
+public class Player(){
+  private String name;
+  private int[2] lastPosition;
+  private ArrayList<ActionTile> actionTiles;
+
+  public void Player(String name, Int[2] lastPosition, ArrayList<ActionTile> actionTiles){
+    this.name = name;
+    this.lastPosition = lastPosition;
+    this.actionTiles = actionTiles;
+  }
+
+  public void moveLeft(){
+
+  }
+
+  public void moveRight(){
+
+  }
+
+  public void moveUp(){
+
+  }
+
+  public void moveDown(){
+
   }
 }
