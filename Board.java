fd712f232bd47a51c413e67a82bd8a274838dfe4
Yannki <jankaji@inmail.sk>
2020-11-16 13:48:08 +0100

    constructor

---
+++ Board.java
@@ -0,0 +1,15 @@
+/**
+ * Board class that is responsible for board and actions
+ * made on it.
+ *
+ * @author Yan Yan Ji
+ * @version 1.0
+ */
+class Board {
+    private static int[][] SIZE;
+
+    public Board (int width, int height){
+        SIZE = new int[width][height];
+    }
+
+}
