cc940e26012d0469e2a9f7950612eea10f039af8
Yannki <jankaji@inmail.sk>
2020-11-16 13:24:16 +0100

    adding empty methods

---
+++ Tile.java
@@ -0,0 +1,16 @@
+
+abstract class Tile {
+    private double chanceOfAppearanceInBag;
+
+    public getTileType() {
+
+    }
+
+    public void executeTile() {
+
+    }
+
+    public double getChangeOfAppearing() {
+
+    }
+}
