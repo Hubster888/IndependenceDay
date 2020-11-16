e99aadcd3e5af3d6ffcda28b6a81a09cce171bb3
Yannki <jankaji@inmail.sk>
2020-11-16 14:42:13 +0100

    constructor

--- Tile.java
+++ Tile.java
@@ -3,6 +3,11 @@ abstract class Tile {
     private double chanceOfAppearanceInBag;
     private final String type;
 
+    public Tile (String type, double chanceOfAppearanceInBag) {
+        this.type = type;
+        this.chanceOfAppearanceInBag = chanceOfAppearanceInBag;
+    }
+
     public String getTileType() {
         return type;
     }
