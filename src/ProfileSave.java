72cd4315c2346c5034ada66dc9e4637b4c5b69e3
Hubert <hubertrzeminski16@gmail.com>
2020-11-17 12:19:27 +0000

    Added and implemented the ProfileSave, all methods work fine except for the update function. I also added a new constructor to the Profiel class. The Profile save still needs tidying up and commenting.

--- src/ProfileSave.java
+++ src/ProfileSave.java
@@ -1,7 +1,26 @@
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import java.io.FileOutputStream;
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class ProfileSave{
 
-  private static Bool fileExists(){
-    File file  = new File("desktop/profileList.txt");
+  private static Boolean fileExists(){
+    File file  = new File("profileList.txt");
     if (file.exists()) {
       return true;
     }else {
@@ -10,50 +29,109 @@ public class ProfileSave{
   }
 
   private static void createFile(){
-    File file  = new File("desktop/profileList.txt");
-    file.createNewFile();
+    File file  = new File("profileList.txt");
+    try{
+      file.createNewFile();
+    }catch(Exception e){
+      System.out.println(e);
+    }
   }
 
-  private static Bool profileExists(Profile profile){
-    File file  = new File("desktop/profileList.txt");
-
+  private static Boolean profileExists(Profile profile){
+    File file  = new File("profileList.txt");
     try {
       Scanner scanner = new Scanner(file);
 
-      //now read the file line by line...
       int lineNum = 0;
       while (scanner.hasNextLine()) {
           String line = scanner.nextLine();
           lineNum++;
-          if(line.contains(profile.getName())) {
+          if(isContain(line, profile.getName())) {
               return true;
             }
           }
         } catch(FileNotFoundException e) {
-          //handle this
+          System.out.println(e);
         }
         return false;
   }
 
   public static void addProfile(Profile profile){
-    if(/*profileExists(profile)*/true){
+    if(fileExists() && profileExists(profile)){
       final JFrame parent = new JFrame();
-      JButton button = new JButton();
 
-      button.setText("Click me to show dialog!");
-      parent.add(button);
       parent.pack();
       parent.setVisible(true);
 
-      String name = JOptionPane.showInputDialog(parent,
-              "What is your name?", null);
+      JOptionPane.showMessageDialog(parent,"This profile already exists!");
+    }else {
+      if(fileExists()){
+
+
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter("profileList.txt", true))) {
+          bw.write(profile.getName() + " 0 0");
+          bw.newLine();
+          bw.close();
+        }catch(Exception e){
+          System.out.println(e);
+        }
+      }else{
+        System.out.println("creating file");
+        createFile();
+        addProfile(profile);
+      }
+
     }
   }
-  // Add profile if not in file
 
-  // Update profile if in file
+  public static void updateProfile(Profile profile, Boolean playerWon){
+      if(profileExists(profile)){
+        try{
+          // input the (modified) file content to the StringBuffer "input"
+          BufferedReader file = new BufferedReader(new FileReader("profileList.txt"));
+          StringBuffer inputBuffer = new StringBuffer();
+          String line;
+
+          while ((line = file.readLine()) != null) {
+            if(isContain(line, profile.getName())){
+              Profile temp = new Profile(profile.getName(), profile.getWins(), profile.getLosses());
+              if(playerWon){temp.addWin();} else {temp.addLoss();}
+              line = temp.getName() + " " + temp.getWins() + " " + temp.getLosses();
+              inputBuffer.append(line);
+              inputBuffer.append('\n');
+            }
+          }
+          file.close();
+
+
+          // write the new string with the replaced line OVER the same file
+          FileOutputStream fileOut = new FileOutputStream("profileList.txt");
+          fileOut.write(inputBuffer.toString().getBytes());
+          fileOut.close();
+        }catch(Exception e){
+          System.out.println(e);
+        }
+
+    }else {
+      final JFrame parent = new JFrame();
+
+      parent.pack();
+      parent.setVisible(true);
+
+      JOptionPane.showMessageDialog(parent,"This profile does not exists!");
+    }
+  }
+
+  private static boolean isContain(String source, String subItem){
+         String pattern = "\\b"+subItem+"\\b";
+         Pattern p=Pattern.compile(pattern);
+         Matcher m=p.matcher(source);
+         return m.find();
+    }
+
   public static void main(String args[]){
-    addProfile()
+    Profile prof = new Profile("huber");
+    updateProfile(prof, true);
   }
 }
 
@@ -62,3 +140,5 @@ public class ProfileSave{
 // eg.
 //Hubert 23 43
 //Jhon 34 1
+
+//Deleates whole file and dosent remember previous stats.
