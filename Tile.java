7f023cc19f2d583ed5be81816073cb0f949c6fd3
Yannki <jankaji@inmail.sk>
2020-11-17 23:34:48 +0100

    first Javadoc

--- Tile.java
+++ Tile.java
@@ -1,20 +1,40 @@
-
+/**
+ * Abstract class for all Tiles in the game.
+ *
+ * @author Yan Yan Ji
+ * @version 1.0
+ */
 abstract class Tile {
     private double chanceOfAppearanceInBag;
     private final String type;
 
-    public Tile (String type, double chanceOfAppearanceInBag) {
+    /**
+     * Constructor
+     * @param type Type of Tile.
+     * @param chanceOfAppearanceInBag Chance of appearing in a game.
+     */
+    public Tile(String type, double chanceOfAppearanceInBag) {
         this.type = type;
         this.chanceOfAppearanceInBag = chanceOfAppearanceInBag;
     }
 
+    /**
+     * @return Type of a Tile.
+     */
     public String getTileType() {
         return type;
     }
 
+    /**
+     * @return Chance of appearing in the game.
+     */
     public double getChangeOfAppearing() {
         return chanceOfAppearanceInBag;
     }
 
+    /**
+     * Abstract method that do the action of a Tile
+     * in a game.
+     */
     public abstract void executeTile();
 }
