3e86d95890286c3bc87712f86a75bbb1c1417d2e
Yannki <jankaji@inmail.sk>
2020-11-21 16:00:57 +0100

    testing which cant be done

---
+++ src/MenuController.java
@@ -0,0 +1,19 @@
+import javafx.fxml.FXML;
+
+import javax.xml.soap.Text;
+import java.awt.*;
+
+public class MenuController {
+    @FXML
+    private Button btnLeaderboard;
+    private Button btnGame;
+    private Button btnSave;
+    private Button btnExit;
+    private Label message;
+
+    public void testButton(Event e){
+
+    }
+
+
+}
