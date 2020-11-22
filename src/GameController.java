3b0f1d186875aa55345dd460897d3219a315be79
Yannki <jankaji@inmail.sk>
2020-11-23 00:50:09 +0100

    Gameboard JavaFx setup

---
+++ src/GameController.java
@@ -0,0 +1,14 @@
+import javafx.fxml.FXML;
+
+import java.awt.*;
+
+public class GameController {
+    @FXML
+    private MenuItem save;
+    private MenuItem load;
+    private MenuItem help;
+    private MenuItem exit;
+    private Canvas canvas;
+
+
+}
