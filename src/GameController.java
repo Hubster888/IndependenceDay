aee206a53ee266ff2e8b1ed044fa3236d838d7cb
Yannki <jankaji@inmail.sk>
2020-11-23 17:35:17 +0100

    exit to Menu button

--- src/GameController.java
+++ src/GameController.java
@@ -1,6 +1,13 @@
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
 
 import java.awt.*;
+import java.io.IOException;
 
 public class GameController {
     @FXML
@@ -9,6 +16,17 @@ public class GameController {
     private MenuItem help;
     private MenuItem exit;
     private Canvas canvas;
+    private BorderPane borderPane;
+
+    public void exitToMenu() throws IOException {
+        FXMLLoader load = new FXMLLoader(getClass().getResource("Menu.fxml"));
+        Parent root = load.load();
+        Scene newScene = new Scene(root);
+        Stage stage = (Stage) borderPane.getScene().getWindow();
+
+        stage.setScene(newScene);
+        stage.show();
+    }
 
     public Canvas getCanvas() {
         return canvas;
