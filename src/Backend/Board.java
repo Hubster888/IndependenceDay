d5dfb5e906e76e58a6a1f5de1af3864f93551a69
RobbieSoutham <45611649+RobbieSoutham@users.noreply.github.com>
2020-12-02 21:35:18 +0000

    Implemented a way of handling level format, currently unchecked.

--- src/Backend/Board.java
+++ src/Backend/Board.java
@@ -1,5 +1,10 @@
 package Backend;
+
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
 
 /**
  * Board class that is responsible for board and actions
@@ -9,10 +14,19 @@ import java.util.ArrayList;
  * @version 1.0
  */
 public class Board {
+    private static final String LEVEL_DIR = "levels/";
+    private static final String GOAL_TILE = "GOAL";
+    private static final String CORNER_TILE = "CORNER";
+    private static final String STRAIGHT_TILE = "STRAIGHT";
+    private static final String TSHAPE_TILE = "TSHAPE";
+    private static final String FILE_DELIM = ",";
+
     private int boardWidth;
     private int boardHeight;
     private ArrayList<Player> listOfPlayers = new ArrayList<Player>();
     private Tile[][] board;
+    private String noOfFloors;
+    private String noOfActions;
 
 
     /**
@@ -20,76 +34,9 @@ public class Board {
      * @param width of board
      * @param height of board
      */
-    public Board(int width, int height, ArrayList<Profile> listOfProfiles) {
-        this.boardWidth = width;
-        this.boardHeight = height;
-        board = new Tile[width][height];
-
-        int xGoal = (int) ((Math.random() * (this.boardHeight - 1) + 1));
-        int yGoal = (int) ((Math.random() * (this.boardHeight - 1)) + 1);
-        board[xGoal][yGoal] = new FloorTile("goal", 0.1, 0);
-        
-        if(listOfProfiles.size() < 0) {
-            System.out.println("Something is wrong, no players");
-        } else {
-
-            for(Profile prof : listOfProfiles) {
-                int[] startingPos = new int[2];
-                int x = -1;
-                int y = -1;
-
-                while((x < 0 || x == xGoal) && (y < 0 || y == yGoal)) {
-                    x = (int) ((Math.random() * (this.boardHeight - 1) + 1));
-                    y = (int) ((Math.random() * (this.boardHeight - 1)) + 1);
-                }
-
-                startingPos[0] = x;
-                startingPos[1] = y;
-                listOfPlayers.add(new Player(prof.getName(), startingPos));
-            }
-
-        }
-
-        for(int i = 0; i < this.boardWidth; i++) {
-            for(int j = 0; j < this.boardHeight; j++) {
-                if(i == 0 && j == 0) {
-                    this.board[i][j] = new FloorTile("corner", 0.1, 0);
-                } else if(i == 0 && j == this.boardHeight - 1) {
-                    this.board[i][j] = new FloorTile("corner", 0.1, 3);
-                } else if(j == 0 && i == this.boardWidth - 1) {
-                    this.board[i][j] = new FloorTile("corner", 0.1, 1);
-                } else if(
-                    j == this.boardHeight - 1 && i == this.boardWidth - 1) {
-                    this.board[i][j] = new FloorTile("corner", 0.1, 2);
-                } else {
-                    int typeGen = (int) ((Math.random() * (4 - 1)) + 1);
-                    int orientationGen = (int) ((Math.random() * (5 - 1)) + 1);
-
-                    switch(typeGen) {
-                        case 1:
-                            this.board[i][j] = new FloorTile("corner", 0.1,
-                            orientationGen);
-                            break;
-                        case 2:
-                            this.board[i][j] = new FloorTile("straight", 0.1,
-                            orientationGen);
-                            break;
-                        case 3:
-                            this.board[i][j] = new FloorTile("tShape", 0.1,
-                            orientationGen);
-                            break;
-                        default:
-                            System.out.println("Something wrong!");
-                            break;
-                    }
-
-                }
-            }
-        }
-
-        board[(int) ((Math.random() * (this.boardHeight - 1) + 1))][(int)
-            ((Math.random() * (this.boardHeight - 1)) + 1)] = new FloorTile(
-            "goal", 0.1, 0);
+    public Board(int levelNo, ArrayList<Profile> listOfProfiles) {
+        ArrayList<String> level = this.getlevel(levelNo);
+        this.setUpLevel(level, listOfProfiles);
     }
 
     public Tile getTile(int x, int y) {
@@ -110,6 +57,14 @@ public class Board {
 
     public Tile[][] getBoard() {
         return this.board;
+    }
+    
+    public String getNoOfFloors() {
+        return noOfFloors;
+    }
+    
+    public String getNoOfActions() {
+        return noOfActions;
     }
     
     public void updateBoard(int rowOrColumn, Boolean isRow,
@@ -138,12 +93,126 @@ public class Board {
     	}
     }
 
-    public static void loadLevel() {
+    public ArrayList<String> getlevel(int levelNo) {
+        ArrayList<String> level = new ArrayList<>();
+        File file = new File(LEVEL_DIR + Integer.toString(levelNo));
         
+        try(Scanner scanner = new Scanner(file)) {
+            while (scanner.hasNextLine())
+                level.add(scanner.nextLine());
+        } catch(FileNotFoundException e) {
+                System.out.println(e);
+                System.exit(0);
+            }
+
+        return level;
     }
 
-    public static void setUpLevel() {
-        
+    /**
+     * Sets up level from given details.
+     * @param level A list of level details.
+     * @param listOfProfiles List of profiles playing the level.
+     */
+    public void setUpLevel(ArrayList<String> level,
+        ArrayList<Profile> listOfProfiles) {
+            ArrayList<ArrayList<String>> levelDetails =
+            new ArrayList<ArrayList<String>>();
+
+            for (String line : level) {
+                levelDetails.add(
+                    (ArrayList<String>) Arrays.asList(line.split(FILE_DELIM))
+                    );
+            }
+
+            // First Line containing the dimensions of the board.
+            this.boardWidth = Integer.parseInt(levelDetails.get(0).get(0));
+            this.boardHeight = Integer.parseInt(levelDetails.get(0).get(1));
+
+            // Second line gives the number of fixed tiles.
+            int fixedTiles = Integer.parseInt(levelDetails.get(1).get(0));
+
+            // Third line gives the number of floor and action tiles to go ito the
+            // silk bag.
+            this.noOfFloors = levelDetails.get(2).get(0);
+            this.noOfActions = levelDetails.get(2).get(0);
+
+            // Lines 4-7 give player starting positions.
+            int[][] spawns = new int[4][2];
+
+            for (int i = 0; i < 4; i++) {
+                spawns[i] = levelDetails.get(4).stream().mapToInt(
+                    Integer::parseInt
+                    ).toArray();
+            }
+
+            // Fixed tile details starts at line 8.
+            for (int i = 8; i < fixedTiles; i++) {
+                
+                // Add the tile to the board with its given details.
+                this.board[Integer.parseInt(
+                    levelDetails.get(i).get(0))][Integer.parseInt(
+                        levelDetails.get(i).get(1))] = new FloorTile(
+                            levelDetails.get(i).get(2), 0.1,
+                            Integer.parseInt(levelDetails.get(0).get(3))
+                            );
+            }
+
+            // Set up the rest of the board tiles.
+            int xGoal = (int) ((Math.random() * (this.boardHeight - 1) + 1));
+            int yGoal = (int) ((Math.random() * (this.boardHeight - 1)) + 1);
+            board[xGoal][yGoal] = new FloorTile(GOAL_TILE, 0.1, 0);
+
+            for (int x = 0; x < this.boardWidth; x++) {
+                for (int y = 0; y < this.boardHeight; y++) {
+                    if (this.board[x][y] != null) {
+                        this.board[x][y] = new FloorTile(
+                            getRandomTileType(), 0.1, (int)((
+                                Math.random() * (5 - 1)) + 1
+                                )
+                            );
+                    }
+                }
+            }
+
+            // Set up players with random position.
+            if(listOfProfiles.size() < 0) {
+                // Throw an error.
+                System.out.println("Something is wrong, no players");
+            } else {
+                for(Profile prof : listOfProfiles) {
+                    int randPos;
+                    int x = -1;
+                    int y = -1;
+
+                    while((x < 0 || x == xGoal) && (y < 0 || y == yGoal)) {
+                        randPos = (int)((Math.random() * 4));
+                        x = spawns[randPos][0];
+                        y = spawns[randPos][1];
+                    }
+ 
+					listOfPlayers.add(new Player(prof.getName(),
+                        spawns[randPos]));
+                }
+            }
+    }
+
+    public static String getRandomTileType() {
+        int typeGen = (int) ((Math.random() * (4 - 1)) + 1);
+        String type;
+
+        switch (typeGen) {
+            case 1:
+                type = CORNER_TILE;
+                break;
+            case 2:
+                type = STRAIGHT_TILE;
+                break;
+            case 3:
+                type = TSHAPE_TILE;
+                break;
+        }
+
+        return type;
     }
 
     /*
@@ -161,6 +230,9 @@ public class Board {
     }*/
 
 }
+
+}
+
 //Add method to say which columns / rows can not move
 //Make the method that takes in as input a row or column and adds a floor tile to that.
 
