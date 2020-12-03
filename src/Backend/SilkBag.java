d0acabc61d603918c582a81999b9cb479f80c604
Yannki <jankaji@inmail.sk>
2020-12-03 14:11:59 +0100

    silk bag

--- src/Backend/SilkBag.java
+++ src/Backend/SilkBag.java
@@ -1,22 +1,44 @@
 package Backend;
+import java.util.Random;
+import java.util.ArrayList;
 /**
- * Silk bag is a class where are stored rest of the
- * tiles from the game that are not in the board or in
- * players' hand.
+ * The Silk Bag class is responsible for holding tiles not on the board or in the Player's hand.
  *
- * @author Yan Yan Ji
+ * @author Mart Krol
  * @version 1.0
  */
-public class SilkBag {
-    private Tile[] tiles;
+class SilkBag {
+
+    private ArrayList<Tile> tiles = new ArrayList<Tile>(); //ArrayList holding the tiles
 
     /**
      * Constructor
-     *
-     * @param tiles List of tiles.
      */
-    public SilkBag(Tile[] tiles) {
-        this.tiles = tiles;
+
+    public SilkBag() {
+    }
+
+    public void fillBag() {
+        Random randTileType = new Random(); //instance of the imported random class
+        int maxType = 4;
+        //generate random int from 0-3
+        int typeNo = randTileType.nextInt(maxType);
+        System.out.println(typeNo);
+
+        Random randOrientation = new Random(); //instance of the imported random class
+        int maxOrientation = 5;
+        //generate random int from 0-4
+        int orientationNo = randOrientation.nextInt(maxOrientation);
+        System.out.println(orientationNo);
+
+        ArrayList<String> tileType = new ArrayList<String>(); //ArrayList holding the types of tiles
+        tileType.add("corner");
+        tileType.add("tShape");
+        tileType.add("straight");
+
+        FloorTile floorTile = new FloorTile(tileType.get(typeNo),0.2, orientationNo); //constructing a floorTile with a random tile type and random orientation
+
+        tiles.add(floorTile);
     }
 
     /**
