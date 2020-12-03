d3ce3ef2b201bbae4a2ef914854a2eb9ab4bf804
Yannki <jankaji@inmail.sk>
2020-12-03 12:24:49 +0100

    action tiles almost finished

--- src/Backend/ActionTile.java
+++ src/Backend/ActionTile.java
@@ -1,11 +1,14 @@
 package Backend;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * Abstract class for all action tiles in a game.
  * @version 1.0
  * @author Yan Yan Ji
  */
-public abstract class ActionTile extends Tile {
+public class ActionTile extends Tile {
     public static final String FIRE = "Fire";
     public static final String ICE = "Ice";
     public static final String DOUBLE_MOVE = "DoubleMove";
@@ -15,7 +18,7 @@ public abstract class ActionTile extends Tile {
         super(type,chanceOfAppearing);
     }
 
-    public void execute(Board board,int col, int row) {
+    public void execute(Board board,Player player, int col, int row) {
         switch (getTileType()){
             case FIRE:
                 fire(board,col,row);
@@ -24,10 +27,10 @@ public abstract class ActionTile extends Tile {
                 ice(board,col,row);
                 break;
             case DOUBLE_MOVE:
-               //Player player = board.getListOfPlayers().get(playerTurn);
-               //actionPlayer(player, col, row);
+               doubleMove(board,player,col,row);
                 break;
             case BACK_TRACK:
+                backTrackMove(board,col,row);
                 break;
             default:
                 System.out.println("Something with action is wrong");
@@ -38,8 +41,8 @@ public abstract class ActionTile extends Tile {
     private void fire(Board board, int col, int row){
         int [] square = setSquare(board,col,row);
 
-        for (int i = square[0]; i < square[1]; i++){
-            for (int j = square[2]; j < square[3]; j++){
+        for (int i = square[0]; i <= square[1]; i++){
+            for (int j = square[2]; j <= square[3]; j++){
                 board.getTile(i,j).setOnFire(true);
             }
         }
@@ -48,9 +51,22 @@ public abstract class ActionTile extends Tile {
     private void ice(Board board, int col, int row){
         int [] square = setSquare(board,col,row);
 
-        for (int i = square[0]; i < square[1]; i++){
-            for (int j = square[2]; j < square[3]; j++){
+        for (int i = square[0]; i <= square[1]; i++){
+            for (int j = square[2]; j <= square[3]; j++){
                 board.getTile(i,j).setFrozen(true);
+            }
+        }
+    }
+
+    private void doubleMove(Board board,Player player, int col, int row){
+       player.move(board,col,row);
+    }
+
+    private void backTrackMove(Board board,int col, int row){
+        ArrayList<Player> players = board.getListOfPlayers();
+        for (Player player: players) {
+            if (Arrays.equals(player.getLastPosition(), new int[]{col, row})){
+                player.setLastPosition(new int[]{0,0});
             }
         }
     }
@@ -65,13 +81,13 @@ public abstract class ActionTile extends Tile {
             indexCol = 0;
         }
         if (col + 1 > board.getWidth()){
-            maxIndexCol = board.getHeight();
+            maxIndexCol = board.getHeight() - 1;
         }
         if (row - 1 < 0){
             indexRow = 0;
         }
         if (row + 1 > board.getHeight()){
-            maxIndexRow = board.getHeight();
+            maxIndexRow = board.getHeight() - 1;
         }
 
         return new int[]{indexCol,maxIndexCol,indexRow,maxIndexRow};
