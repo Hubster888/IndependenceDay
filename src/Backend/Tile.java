97bb7e92b84be5804d814f1c91c24f36334571a4
Yannki <jankaji@inmail.sk>
2020-12-04 23:18:15 +0100

    auto clean up

--- src/Backend/Tile.java
+++ src/Backend/Tile.java
@@ -1,17 +1,18 @@
 package Backend;
+
 /**
  * Abstract class for all Tiles in the game.
  *
  * @author Yan Yan Ji
  * @version 1.0
  */
-
 public abstract class Tile {
 
     private final String type;
 
     /**
      * Constructor
+     *
      * @param type Type of Tile.
      */
     public Tile(String type) {
