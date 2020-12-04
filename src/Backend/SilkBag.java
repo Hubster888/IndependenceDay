440db575bb7254787214c3d06e7bf4c8eaeb1de0
Yannki <jankaji@inmail.sk>
2020-12-04 21:16:32 +0100

    silkbag finish

--- src/Backend/SilkBag.java
+++ src/Backend/SilkBag.java
@@ -2,6 +2,8 @@ package Backend;
 
 import java.util.Random;
 import java.util.ArrayList;
+import java.util.LinkedList;
+
 
 /**
  * The Silk Bag class is responsible for holding tiles not on the board or in the Player's hand.
@@ -11,7 +13,7 @@ import java.util.ArrayList;
  */
 public class SilkBag {
 
-    private ArrayList<Tile> tiles = new ArrayList<Tile>(); //ArrayList holding the tiles
+    private LinkedList<Tile> tiles = new LinkedList<Tile>(); //Linked list holding the tiles
 
     /**
      * Constructor
@@ -20,7 +22,24 @@ public class SilkBag {
     public SilkBag() {
     }
 
-    public void fillBag() {
+    private void generateFloorTiles(String tileType, int orientationNo) {
+
+        FloorTile floorTile = new FloorTile(tileType, orientationNo);
+        tiles.add(floorTile);
+
+    }
+
+    private void generateActionTiles(String tileType) {
+
+        ActionTile actionTile = new ActionTile(tileType);
+        tiles.add(actionTile);
+
+    }
+
+    /**
+     *
+     */
+    public void fillBag(int noFloorTiles, int noActionTiles) {
 
         ArrayList<String> tileType = new ArrayList<String>(); //ArrayList holding the types of tiles
         tileType.add("corner");
@@ -28,40 +47,47 @@ public class SilkBag {
         tileType.add("straight");
 
         Random randTileType = new Random(); //instance of the imported random class
-        int typeNo = randTileType.nextInt(tileType.size());
-        System.out.println(typeNo);
-
         Random randOrientation = new Random(); //instance of the imported random class
-        int maxOrientation = 5;
-        //generate random int from 0-4
-        int orientationNo = randOrientation.nextInt(maxOrientation);
-        System.out.println(orientationNo);
 
-        FloorTile floorTile = new FloorTile(tileType.get(typeNo), orientationNo); //constructing a floorTile with a random tile type and random orientation
+        for(int i = 0; i < noFloorTiles+1; i++){
+            int typeNo = randTileType.nextInt(tileType.size());
+            String tileTypeResult = tileType.get(typeNo);
 
-        tiles.add(floorTile);
+            int maxOrientation = 5;
+            int orientationNo = randOrientation.nextInt(maxOrientation);
 
-//        ArrayList<Tile> actionTileType = new ArrayList<Tile>();
-//
-//        FireTile fireTile = new FireTile();
-//        actionTileType.add(fireTile);
-//        IceTile iceTile = new IceTile();
-//        actionTileType.add(iceTile);
-//        DoubleMoveTile doubleMoveTile = new DoubleMoveTile();
-//        actionTileType.add(doubleMoveTile);
-//        BackTrackTile backTrackTile = new BackTrackTile();
-//        actionTileType.add(backTrackTile);
+            generateFloorTiles(tileTypeResult, orientationNo);
+        }
 
+        ArrayList<String> actionTileType = new ArrayList<String>();
+        actionTileType.add("Fire");
+        actionTileType.add("Ice");
+        actionTileType.add("DoubleMove");
+        actionTileType.add("BackTrack");
+
+        Random randActionTile = new Random();
+        for(int i = 0; i < noActionTiles+1; i++){
+            int actionNo = randActionTile.nextInt(actionTileType.size());
+            String tileTypeResult = actionTileType.get(actionNo);
+
+            generateActionTiles(tileTypeResult);
+        }
     }
 
     /**
      * Method that will draw tiles in canvas.
      */
+
     public Tile drawTile() {
         Random randTile = new Random(); //instance of the imported random class
         int tileNo = randTile.nextInt(tiles.size());
         Tile tileDraw = tiles.get(tileNo);
+        tiles.remove(tileNo);
 
         return (tileDraw);
+    }
+
+    public void addTile(String tileType, int orientationNo) {
+        generateFloorTiles(tileType, orientationNo);
     }
 }
