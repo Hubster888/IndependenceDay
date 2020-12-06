0f1d1e4a345abe16e5e6b65ff8008478fcf0984a
Yannki <jankaji@inmail.sk>
2020-12-06 12:31:19 +0100

    Javadoc

--- src/Main.java
+++ src/Main.java
@@ -8,13 +8,30 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/**
+ * Main class that will start the game.
+ *
+ * @author Group 36
+ * @version 1.0
+ */
 public class Main extends Application {
+    private static final String GAME_FXML = "Frontend/Game.fxml";
+    private static final String MENU_FXML = "Frontend/Menu.fxml";
+    private static final String TITLE = "Independence Day";
 
+    /**
+     * Launch the game.
+     * @param args Command line input.
+     */
     public static void main(String[] args) {
         launch(args);
     }
 
-
+    /**
+     * Start of the game.
+     * @param stage Stage where the game will be shown.
+     * @throws IOException Input file error.
+     */
     @Override
     public void start(Stage stage) throws IOException {
         File dataPersistence = new File(Save.DATA_PERSISTENCE);
@@ -22,13 +39,13 @@ public class Main extends Application {
 
         //If there is unfinished game, it will load that one
         if (dataPersistence.exists()){
-            main = FXMLLoader.load(getClass().getResource("Frontend/Game.fxml"));
+            main = FXMLLoader.load(getClass().getResource(GAME_FXML));
         } else {
-            main = FXMLLoader.load(getClass().getResource("Frontend/Menu.fxml"));
+            main = FXMLLoader.load(getClass().getResource(MENU_FXML));
         }
 
 
-        stage.setTitle("Dev Launcher");
+        stage.setTitle(TITLE);
 
         Scene scene = new Scene(main);
 
